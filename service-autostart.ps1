e393d839b049279aa5696595ec4ca27ff04646bd
Smartworx Praktikanten <Praktikant@smartworx.de>
2024-06-25 09:20:58 +0200

    Starten des gestoppten Dienst + Fehlerbehandlung

--- service-autostart.ps1
+++ service-autostart.ps1
@@ -8,8 +8,19 @@ if ($services.Length -ge 1) {
     for ($i = 0; $i -lt $services.Length; $i++) {
         Write-Host $services[$i].Name
     }
-
+    Write-Host ""
+    for ($j = 0; $j -lt $services.Length; $j++) {
+        try {
+            Start-Service -InputObject $services[$j] -ErrorAction Stop
+        }
+        catch {
+            Write-Host $_.Exception.Message
+        }
+    }
 }
 else {
     Write-Host "Alle Dienste wurde Ordnungsgemäß gestartet."
 }
+
+Write-Host ""
+Write-Host "Die Überprüfung wurde beendet."

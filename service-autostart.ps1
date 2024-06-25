88763ff1c08d209d54b7dd12806af68ad5c9e62a
Smartworx Praktikanten <Praktikant@smartworx.de>
2024-06-25 11:22:06 +0200

    exclusionlist.txt hinzugefügt und in service-autostart.ps1 eingebunden

--- service-autostart.ps1
+++ service-autostart.ps1
@@ -1,15 +1,17 @@
-﻿Write-Host "Überprüfung gestartet"
+﻿Write-Host ""
+Write-Host "Überprüfung gestartet"
 
-$services = Get-Service | Where-Object {$_.StartType -eq "Automatic"} | Where-Object {$_.Status -eq "Stopped"} | Where-Object {$_.DisplayName -notmatch "Google"} 
+$exclusion = Get-Content "exclusionlist.txt" | Select-Object -Skip 1
+$services = Get-Service | Where-Object {$_.StartType -eq "Automatic"} | Where-Object {$_.Status -eq "Stopped"} | Where-Object {$_.DisplayName -notmatch "Google"} | Where-Object {$_.DisplayName -notin $exclusion}
 
-if ($services.Length -ge 1) {
+if ($services.Count -ge 1) {
     Write-Host "Name aller Dienste die nicht gestartet wurden:"
-    
-    for ($i = 0; $i -lt $services.Length; $i++) {
+    for ($i = 0; $i -lt $services.Count; $i++) {
+        
         Write-Host $services[$i].Name
     }
     Write-Host ""
-    for ($j = 0; $j -lt $services.Length; $j++) {
+    for ($j = 0; $j -lt $services.Count; $j++) {
         try {
             Start-Service -InputObject $services[$j] -ErrorAction Stop
         }
@@ -22,5 +24,4 @@ else {
     Write-Host "Alle Dienste wurde Ordnungsgemäß gestartet."
 }
 
-Write-Host ""
 Write-Host "Die Überprüfung wurde beendet."

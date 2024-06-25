9934124134a185016e0d314a118ce0aad65d3395
Smartworx Praktikanten <Praktikant@smartworx.de>
2024-06-25 08:23:36 +0200

    Überprüfung der Dienste die nicht gestartet wurde und die Ausgabe der Namen

---
+++ service-autostart.ps1
@@ -0,0 +1,15 @@
+﻿Write-Host "Überprüfung gestartet"
+
+$services = Get-Service | Where-Object {$_.StartType -eq "Automatic"} | Where-Object {$_.Status -eq "Stopped"}
+
+if ($services.Length -ge 1) {
+    Write-Host "Name aller Dienste die nicht gestartet wurden:"
+    
+    for ($i = 0; $i -lt $services.Length; $i++) {
+        Write-Host $services[$i].Name
+    }
+
+}
+else {
+    Write-Host "Alle Dienste wurde Ordnungsgemäß gestartet."
+}
